3336e32d8c5971aaeb3117bf05fd7e9f141000c8
Hamza Riaz <60283257+SheikhHamxa@users.noreply.github.com>
2023-01-31 18:49:43 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit
 streamlit.title('My Parents New haelthy')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text( 🥣'Omega 3 & Blueberry Oatmeal')
+streamlit.text(🥗 🐔'Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+  🥑🍞
