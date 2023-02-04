4280753f2feb0d15734f6002b415916d18e5c83e
Hamza Riaz <60283257+SheikhHamxa@users.noreply.github.com>
2023-02-04 14:05:35 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,5 +31,19 @@ streamlit.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do? 
+#### it will show normalize data, mean in table form, before it was simple line inverted commas 
+streamlit.dataframe(fruityvice_normalized)
+
+
+###### We can pass input of name in variable lets see below
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit"+ fruit_choice)
+streamlit.text(fruityvice_response.json())
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do? 
 #### it will show normalize data
 streamlit.dataframe(fruityvice_normalized)
