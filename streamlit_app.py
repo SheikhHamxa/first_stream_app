18716aadd7516f46556501f307341998c1b75700
Hamza Riaz <60283257+SheikhHamxa@users.noreply.github.com>
2023-02-06 19:29:59 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,14 +61,15 @@ streamlit.dataframe(fruityvice_normalized)
 #### lets introduce step of try except --- mean value already got enter wont go again
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-  ("please select a valid fruit")
-else:
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  streamlit.dataframe(fruityvice_normalized)
+  if not fruit_choice:
+    ("please select a valid fruit")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+  
 
-  except URLError as e:
+except URLError as e:
     streamlit.error()
 
   
